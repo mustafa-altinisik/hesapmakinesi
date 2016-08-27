670e8069c72c5b9a391bcb40796c0c7d001802e9
Mustafa Asım Altınışık <mustafa@altinisik.net>
2016-08-27 17:52:37 +0300

    gpio interrupt eklendi

--- makinem.py
+++ makinem.py
@@ -209,13 +209,12 @@ class Deneme(App):
         if (self.ledStatus==False):
             self.cikti.text = self.cikti.text + "b"
             GPIO.output(ledPin, GPIO.HIGH)
+            self.ledStatus = True
 
         else:
             self.cikti.text = self.cikti.text + "c"
             GPIO.output(ledPin, GPIO.LOW)
-
-
-
+            self.ledStatus = False
 
     def build(self):
         self.sontus=""
